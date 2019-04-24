eb96d0131a4428cc9ab93205b5f426e583c24df5
Matvey Mitnitsky <matvey.m@openlegacy.com>
2019-04-24 15:09:17 +0300

    Initial commit

---
+++ examples/find/java/CarBuilder.java
@@ -0,0 +1,10 @@
+/**
+ * The builder abstraction.
+ */
+interface CarBuilder {
+    Car build();
+
+    CarBuilder setColor(final String color);
+
+    CarBuilder setWheels(final int wheels);
+}
---
+++ examples/find/java/Singleton.java
@@ -0,0 +1,10 @@
+public final class Singleton {
+
+    private static final Singleton INSTANCE = new Singleton();
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        return INSTANCE;
+    }
+}
